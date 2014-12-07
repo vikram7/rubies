31ba3eccd398766be2f57312b7177018af9fa171
Vikram Ramakrishnan <lord.ezar@gmail.com>
2014-12-07 16:09:12 -0500

    update classes

--- lib/launch_drills.rb
+++ lib/launch_drills.rb
@@ -11,19 +11,6 @@ module LaunchDrills
   # Your code goes here...
 end
 
-class Hash
-  def deep_traverse(&block)
-    stack = self.map{ |k,v| [ [k], v ] }
-    while not stack.empty?
-      key, value = stack.pop
-      yield(key, value)
-      if value.is_a? Hash
-        value.each{ |k,v| stack.push [ key.dup << k, v ] }
-      end
-    end
-  end
-end
-
 def data_structure
   choice = rand(1..3)
   case choice
--- lib/random_hash.rb
+++ lib/random_hash.rb
@@ -1,3 +1,16 @@
+class Hash
+  def deep_traverse(&block)
+    stack = self.map{ |k,v| [ [k], v ] }
+    while not stack.empty?
+      key, value = stack.pop
+      yield(key, value)
+      if value.is_a? Hash
+        value.each{ |k,v| stack.push [ key.dup << k, v ] }
+      end
+    end
+  end
+end
+
 class RandomHash < Hash
   def initialize
     @ds = Hash.new
