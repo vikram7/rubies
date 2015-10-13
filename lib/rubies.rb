da3308f7ac27712e12b14873634e140a41f6c676
Chris C Cerami <chrisccerami@gmail.com>
2015-10-12 20:01:17 -0400

    Address Hound comments

--- lib/rubies.rb
+++ lib/rubies.rb
@@ -1,9 +1,9 @@
 # require "rubies/version"
-require 'colorize'
-require 'awesome_print'
-require 'pp'
-require 'rubies/random_array'
-require 'rubies/random_hash'
-require 'rubies/random_data_structure'
-require 'rubies/game'
-require 'ripper'
+require "colorize"
+require "awesome_print"
+require "pp"
+require "rubies/random_array"
+require "rubies/random_hash"
+require "rubies/random_data_structure"
+require "rubies/game"
+require "ripper"
