1cfc1101b8e1cc6e03a2195210526c7fddc6ec80
Vikram Ramakrishnan <lord.ezar@gmail.com>
2016-02-15 00:16:35 -0500

    add all_values for MyHash

--- spec/rubies/random_data_structure_spec.rb
+++ spec/rubies/random_data_structure_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require "faker"
 
 module Rubies
   describe RandomDataStructure do
@@ -50,7 +51,42 @@ module Rubies
     end
 
     it "#all_values returns expected array for MyHash" do
-      # pending
+      hash = Rubies::MyHash.new
+      2.times do |num|
+        details = Hash.new
+        details["fox"] = "mulder#{num}"
+        details["dana"] = "scully#{num}"
+        details["phone"] = "123-456-789#{num}"
+        details["company"] = "FBI#{num}"
+        hash["xfiles#{num}"] = details
+      end
+      @random_data_structure.data_structure = hash
+      values = []
+      hash.deep_traverse { |_path, value| values << value }
+      expected =
+        [
+          {
+            "fox"=>"mulder1",
+            "dana"=>"scully1",
+            "phone"=>"123-456-7891",
+            "company"=>"FBI1"
+          },
+          "FBI1",
+          "123-456-7891",
+          "scully1",
+          "mulder1",
+          {
+            "fox"=>"mulder0",
+            "dana"=>"scully0",
+            "phone"=>"123-456-7890",
+            "company"=>"FBI0"
+          },
+          "FBI0",
+          "123-456-7890",
+          "scully0",
+          "mulder0"
+        ]
+      expect(values).to eq(expected)
     end
   end
 end
