aa9ef5a688d192affcd9d88492d11887e88da519
Vikram Ramakrishnan <lord.ezar@gmail.com>
2016-02-16 22:06:24 -0500

    add string case

--- spec/rubies/game_spec.rb
+++ spec/rubies/game_spec.rb
@@ -16,7 +16,7 @@ module Rubies
     it "#generate_data_structure returns a data structure and target" do
       output = @game.generate_data_structure
       expect(is_hash_my_hash_or_array(output.first)).to eq(true)
-      expect(is_hash_or_fixnum(output.last)).to eq(true)
+      expect(is_hash_or_fixnum_or_string(output.last)).to eq(true)
     end
   end
 end
@@ -26,7 +26,7 @@ def is_hash_my_hash_or_array(data_structure)
   [Hash, Rubies::MyHash, Array].any?(&klass.method(:==))
 end
 
-def is_hash_or_fixnum(value)
+def is_hash_or_fixnum_or_string(value)
   klass = value.class
-  [Hash, Fixnum].any?(&klass.method(:==))
+  [Hash, Fixnum, String].any?(&klass.method(:==))
 end
